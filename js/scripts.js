68d57d962b99e540677b6d694275c47db75cd346
raffaelab <raffaela.brunner@gmail.com>
2020-05-30 22:10:06 +0200

    Upload of my files for project 5

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$('#search').on('keyup', function() {
+  const $userSearch = $('#search').val().toLowerCase();
+  $('.gallery a').each(function () {
+    const $caption = $(this).attr('data-title').toLowerCase();
+    const match = $caption.includes($userSearch);
+    if (match === true) {
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
+});
+
+lightbox.option({
+  'maxWidth': 800,
+  'positionFromTop': 100,
+  'showImageNumberLabel': false,
+});
